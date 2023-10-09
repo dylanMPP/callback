757c8c9fae73daaaec49e0e8e318b854ea2ed755
Dylan Bermudez Cardona <dyez1110@gmail.com>
2023-10-09 17:52:16 -0500

    feat(server & client): Add configuration with adapter and running

--- simulate_clients.sh
+++ simulate_clients.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
 # Número de clientes a simular
-NUM_CLIENTS=5
+NUM_CLIENTS=30
+# Número de solicitudes por cliente
+NUM_REQUESTS=1000
 
 # Dirección y puerto del servidor
 SERVER_HOST="localhost"
 SERVER_PORT=8000
 
-java -jar server/build/libs/server.jar &
-
 # Función para simular un cliente
 simulate_client() {
- echo -e "1300"| java -jar client/build/libs/client.jar &
+    for ((i = 1; i <= NUM_REQUESTS; i++)); do
+        echo -e "10000000000000"| java -jar client/build/libs/client.jar
+    done
 }
 
 
 # Esperar a que el servidor se inicie
+
 sleep 3
 
-# Simular múltiples clientes
+# Simular múltiples clientes en segundo plano y almacenar los PID en un arreglo
+declare -a CLIENT_PIDS
 for ((i = 1; i <= NUM_CLIENTS; i++)); do
- simulate_client "$1"
-
+    simulate_client "$i" &
+    CLIENT_PIDS+=($!)  # Almacena el PID del cliente en el arreglo
 done
- echo -e "exit"| java -jar client/build/libs/client.jar
+
 # Esperar a que los clientes completen
-wait
+for pid in "${CLIENT_PIDS[@]}"; do
+    wait $pid
+done
+
+pkill -f "java -jar server/build/libs/server.jar"
